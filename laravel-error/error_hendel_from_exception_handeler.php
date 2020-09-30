c0f3341326aef5bcba216125bc933f6190790d67
Qayum Hasan <45765971+qayumhasan@users.noreply.github.com>
2020-09-30 17:53:50 +0600

    Update error_hendel_from_exception_handeler.php

--- laravel-error/error_hendel_from_exception_handeler.php
+++ laravel-error/error_hendel_from_exception_handeler.php
@@ -1,1 +1,22 @@
 
+ /**
+     * Render an exception into an HTTP response.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Exception  $exception
+     * @return \Illuminate\Http\Response
+     */
+    public function render($request, Exception $exception)
+    {
+
+     
+
+        // error handler code
+        if($this->isHttpException($exception)){
+            if($exception->getStatusCode() == 404){
+                return response()->view('frontend.errors.404');
+            }
+        }
+       
+        return parent::render($request, $exception);
+    }
