278c0bf069fa4cab3bb14a82bfa8b34a8ad47e0d
Qayum Hasan <45765971+qayumhasan@users.noreply.github.com>
2020-11-22 11:46:07 +0600

    Create services.php

---
+++ Socialite/services.php
@@ -0,0 +1,5 @@
+ 'facebook' => [
+        'client_id' => env('FACEBOOK_CLIENT_ID'),
+        'client_secret' => env('FACEBOOK_CLIENT_SECRET'),
+        'redirect' => 'https://localhost:8000/auth/facebook/callback',
+    ],
