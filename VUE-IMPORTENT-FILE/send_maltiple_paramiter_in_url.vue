1fb411132086d37df348caa6746fc6372f2e02ec
Qayum Hasan <45765971+qayumhasan@users.noreply.github.com>
2021-06-05 16:58:51 +0600

    Create send_maltiple_paramiter_in_url.vue

---
+++ VUE-IMPORTENT-FILE/send_maltiple_paramiter_in_url.vue
@@ -0,0 +1,11 @@
+//in route
+
+ {
+        path: '/email_varify/:email/:id',                                     
+        component: require('./components/auth/email_varify').default,
+        name:'email_varify'
+    },
+    
+ //in vue template
+ this.$router.push({ name: 'email_varify', params:{email:"dsafsad",id:'10'}});
+ 
