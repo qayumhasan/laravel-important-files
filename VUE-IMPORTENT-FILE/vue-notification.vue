6660460ccc759fb7570f06aab0d635d99ece32a3
Qayum Hasan <45765971+qayumhasan@users.noreply.github.com>
2021-06-06 16:38:16 +0600

    Create vue-notification.vue

---
+++ VUE-IMPORTENT-FILE/vue-notification.vue
@@ -0,0 +1,19 @@
+## Install vue notification
+
+  npm install --save vue-notification
+
+## In app.js file
+
+import Notifications from 'vue-notification'
+Vue.use(Notifications)
+
+## In Login.vue file
+
+this.$notify({
+  title: 'Important message',
+  text: 'Hello user!'
+});
+
+
+##  Write it on the top of the main file
+<notifications/>
